edac30e84325e77c08ae5569df21e291fe7a4f50
Aaron Barnes <aaron.barnes1@hotmail.com>
2021-05-15 16:41:56 -0500

    small change to spacing

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -1,9 +1,7 @@
 # > bubble_sort([4,3,78,2,0,2])
 # => [0,2,2,3,4,78]
-
-#numbers = [4,3,78,2,0,2]
 numbers = [4,3,9,57,1,11,78,2,0,2,9]
-words = ["pizza", "milk", "milkshake"]
+words = ["pizza", "milk", "milkshake", "eyeballs", "pancakes"]
 
 def bubble_sort(numbers)
   sorted = Array.new(numbers)
@@ -12,8 +10,8 @@ def bubble_sort(numbers)
   pass = 1
 
   until pass > max_passes do
-
     sorted[0..-pass].each_index do |index| #only deal with items not yet sorted
+      
       unless index == sorted[0..-pass].length - 1 #stop on last item as nothing left to compare
         #switch items if left > right
         if sorted[index] > sorted[index + 1]
