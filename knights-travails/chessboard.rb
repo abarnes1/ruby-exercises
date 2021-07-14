5fd65fe8e4c07e7435b31bb4e0ef5509ab00ed26
Aaron Barnes <aaron.barnes1@hotmail.com>
2021-07-14 11:46:35 -0500

    retreive square by coord or algebraic notation

--- knights-travails/chessboard.rb
+++ knights-travails/chessboard.rb
@@ -6,14 +6,19 @@ class Chessboard
   def initialize
     @squares = Array.new(64) { ChessSquare.new }
     init_squares
-    # @x = x_size
-    # @y = y_size
   end
 
-  def square(x, y)
-    index = x + (y * 8)
-    # puts "\n#{x}, #{y} is index #{index}, obj id #{@squares[index].object_id}\n"
-    @squares[index]
+  def square(location)
+    square = nil
+
+    if location.instance_of?(Array)
+      puts 'loc is array'
+      square = @squares[coord_to_index(location)]
+    elsif location.instance_of?(String)
+      square = @squares[algebraic_to_index(location)]
+    end
+
+    square
   end
 
   def console_print
@@ -36,6 +41,35 @@ class Chessboard
     puts board
   end
 
+  def index_to_algebraic(index)
+    row = 8 - (index / 8)
+    col = (index % 8) + 97
+    puts "index: #{index} - #{col.chr}#{row}"
+    "#{col.chr}#{row}"
+  end
+
+  def index_to_coord(index)
+    row = index % 8
+    col = index / 8
+    puts "index: #{[row, col]}"
+    [row, col]
+  end
+
+  def algebraic_to_index(location)
+    x_coord = location[0].ord - 97
+    y_coord = (location[1].to_i - 8).abs
+    index = coord_to_index([x_coord, y_coord])
+    puts "coord: #{location} is index #{index}"
+
+    index
+  end
+
+  def coord_to_index(coord)
+    index = coord[0] + (coord[1] * 8)
+    puts "#{coord[0]}, #{coord[1]} is index #{index}, obj id #{@squares[index].object_id}"
+    index
+  end
+
   def init_squares
     @squares.each_index do |index|
       if (index + (index / 8)).even?
@@ -46,5 +80,3 @@ class Chessboard
     end
   end
 end
-
-
